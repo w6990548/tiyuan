03336ea7f6e21171057b01d993d552886dbdc499
FengLei <www.409050716@foxmail.com>
2020-08-05 16:24:37 +0800

    修复文章编辑页不显示内容的问题

--- resources/js/components/admin/article/form.vue
+++ resources/js/components/admin/article/form.vue
@@ -79,7 +79,7 @@
             initForm(editData) {
                 this.form.id = editData.id;
                 this.form.title = editData.title;
-                this.form.contents = editData.content ? editData.contents : '';
+                this.form.contents = editData.content ? editData.content : '';
                 editData.labels.forEach(item => {
                     this.form.labels.push(item.id);
                 })
